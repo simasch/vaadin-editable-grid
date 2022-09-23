167e4c1e2fcdde6b2d7f89b58a01db33a250034a
start.vaadin.com <no-reply@vaadin.com>
2022-09-23 06:14:11 +0000

    Generated project

---
+++ src/main/java/ch/martinelli/demo/data/service/SamplePersonRepository.java
@@ -0,0 +1,9 @@
+package ch.martinelli.demo.data.service;
+
+import ch.martinelli.demo.data.entity.SamplePerson;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
+
+}
